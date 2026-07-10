d627efa090e262a1355a0c2503370b5ee60e7df5
Kinnay21 <legrandyannick21@laposte.net>
2026-07-10 07:52:07 +0200

    save first step

--- scenes/angry_pig.gd
+++ scenes/angry_pig.gd
@@ -2,70 +2,114 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-
-const SPEED = 300.0
+const RUNNING = 300.0
 const WALK = 50.0
+# Ignore tiny left/right differences to avoid jitter when player is almost aligned.
+const TURN_DEADZONE_X = 12.0
+# Minimum delay between two direction flips for smoother tracking behavior.
+const TURN_COOLDOWN_SECONDS = 0.5
+var health := 5
+var player: CharacterBody2D
+var character_velocity = WALK
 
-var is_going_to_right: bool = true
+var is_hit: bool = false
+var hit_animation: String = "hit1"
+const DAMAGE_COOLDOWN_SECONDS = 0.25
+@onready var damage_cooldown_timer: Timer = $DamageCooldownTimer
+
+var is_going_to_right: bool = false
 var is_running: bool = false
+var turn_cooldown_left := 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	damage_cooldown_timer.one_shot = true
+	damage_cooldown_timer.wait_time = DAMAGE_COOLDOWN_SECONDS
+	damage_cooldown_timer.timeout.connect(_on_damage_cooldown_timer_timeout)
+
 
 func _physics_process(delta):
-	
 	# sprite animation of the character
 	animate_character()
 	# vitesse movement of the character on X and Y
 	character_movement(delta)
 	
-	
 func animate_character():
-	
 	#animate character on the floor
 	if (velocity.x > 1 || velocity.x < -1):
-		if is_running:
+		if is_hit:
+			animated_sprite_2d.animation = hit_animation
+		elif is_running:
 			animated_sprite_2d.animation = "running"
 		else:
 			animated_sprite_2d.animation = "walking"
-	if velocity.x < 1 and velocity.x > -1: 
+	else: 
 		animated_sprite_2d.animation = "default"
+	
+	# be sure to play animation
+	animated_sprite_2d.play()
 	
 	# flip animation
 	animated_sprite_2d.flip_h = is_going_to_right
 
-
 func character_movement(delta):
+	# Countdown that throttles how often the pig is allowed to turn around.
+	if turn_cooldown_left > 0.0:
+		turn_cooldown_left = max(0.0, turn_cooldown_left - delta)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	
-	var char_velocity = WALK
-	if is_running:
-		char_velocity = SPEED
+	if player:
+		character_velocity = RUNNING
+		is_running = true
+		var dir = player.position.x - position.x
+		# Turn only when player is clearly on one side and cooldown is finished.
+		if absf(dir) > TURN_DEADZONE_X and turn_cooldown_left <= 0.0:
+			var should_go_right = dir > 0
+			if should_go_right != is_going_to_right:
+				is_going_to_right = should_go_right
+				# Start cooldown to prevent rapid flip-flop at the boundary.
+				turn_cooldown_left = TURN_COOLDOWN_SECONDS
 	else:
-		char_velocity = WALK
+		character_velocity = WALK
+		is_running = false
 	
 	if is_going_to_right:
 		var direction = 1
-		velocity.x = direction * char_velocity
+		velocity.x = direction * character_velocity
 	else:
 		var direction = -1
-		velocity.x = direction * char_velocity
+		velocity.x = direction * character_velocity
 	
 	move_and_slide()
 	
 	if velocity.x < 1 and velocity.x > -1:
 		is_going_to_right = !is_going_to_right
+	
+	
 
+func _on_enemi_area_body_entered(player_body: CharacterBody2D) -> void:
+	player = player_body
 
-func running_mode(is_going_to_run:bool, is_player_area_right:bool):
-	if is_going_to_run == true:
-		if is_going_to_right == is_player_area_right:
-			is_running = true
-		else:
-			is_running = false
-	else:
-		is_running = false
+func _on_enemi_area_body_exited(_player_body: CharacterBody2D) -> void:
+	player = null
+
+func get_damage():
+	if not damage_cooldown_timer.is_stopped():
+		return
+
+	print("damage angry_pig")
+	is_hit = true
+	hit_animation = ["hit1", "hit2"].pick_random()
+	health -= 1
+	damage_cooldown_timer.start()
+	if health <= 0:
+		queue_free()
+	
+func _on_damage_cooldown_timer_timeout():
+	if is_hit:
+		is_hit = false
--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const RUNNING = 400.0
+const JUMP_VELOCITY = -800.0
+const DAMAGE_BOUNCE = 900.0
 
 const ACC = 50
 const FRICTION = 70
@@ -10,27 +11,35 @@ const FRICTION = 70
 const WALL_JUMP_PUSHBACK = 500
 const WALL_SLIDE_GRAVITY = 100
 
+
 var is_wall_slinding: bool = false
-
 var is_going_to_left: bool = false
-
 var can_double_jump: bool = true
+var health := 5
 
 @onready var sprite_2d = $Sprite2D
+
+var is_invulnerable: bool = false
+var is_hit: bool = false
+const DAMAGE_COOLDOWN_SECONDS = 0.3
+@onready var damage_cooldown_timer: Timer = $DamageCooldownTimer
 
 
 var chosen_character = 1
 var format_string_sprite_name = "%s_%s"
 
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _ready():
+	damage_cooldown_timer.one_shot = true
+	damage_cooldown_timer.wait_time = DAMAGE_COOLDOWN_SECONDS
+	damage_cooldown_timer.timeout.connect(_on_damage_cooldown_timer_timeout)
+
 func _physics_process(delta):
-	
+	# change character
 	choose_character()
-	
 	# vitesse movement of the character on X and Y
 	character_movement(delta)
 	# sprite animation of the character
@@ -40,10 +49,10 @@ func _physics_process(delta):
 	# jump action of the character
 	jump()
 
-
-
 func animate_character():
-	if is_on_floor():
+	if is_hit:
+		sprite_2d.animation = format_string_sprite_name % ["hit", chosen_character]
+	elif is_on_floor():
 		#animate character on the floor
 		if (velocity.x > 1 || velocity.x < -1):
 			sprite_2d.animation = format_string_sprite_name % ["running",chosen_character]
@@ -66,7 +75,7 @@ func animate_character():
 				sprite_2d.animation = format_string_sprite_name % ["falling",chosen_character]
 					
 	sprite_2d.flip_h = is_going_to_left
-				
+
 func character_movement(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -76,8 +85,8 @@ func character_movement(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-#		velocity.x = direction * SPEED
-		velocity.x = move_toward(velocity.x, direction * SPEED, ACC)
+#		velocity.x = direction * RUNNING
+		velocity.x = move_toward(velocity.x, direction * RUNNING, ACC)
 		if direction == 1:
 			is_going_to_left = false
 		else:
@@ -86,8 +95,12 @@ func character_movement(delta):
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
 		
 	move_and_slide()
-	
-	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is Node and collider.is_in_group("Ennemi"):
+			#velocity += 50 * collision.get_normal()
+			damage(collision, collider)
 
 func wall_slide(delta):
 	# slide on wall
@@ -101,7 +114,7 @@ func wall_slide(delta):
 	if is_wall_slinding:
 		velocity.y += WALL_SLIDE_GRAVITY * delta
 		velocity.y = min(velocity.y, WALL_SLIDE_GRAVITY)
-		
+
 func jump():
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
@@ -120,12 +133,6 @@ func jump():
 				can_double_jump = false
 
 func choose_character():
-	# exampel to format string :
-	#	# Define a format string with placeholder '%s'
-	#	var format_string = "We're waiting for %s."
-	#	# Using the '%' operator, the placeholder is replaced with the desired value
-	#	var actual_string = format_string % chosen_character
-	#	print(actual_string)
 	if Input.is_action_just_pressed("pad_number_1"):
 		chosen_character = 1
 	if Input.is_action_just_pressed("pad_number_2"):
@@ -134,6 +141,31 @@ func choose_character():
 		chosen_character = 3
 	if Input.is_action_just_pressed("pad_number_4"):
 		chosen_character = 4
-		
-	
 
+func damage(collision:KinematicCollision2D, enemy:Object):
+	
+	if is_invulnerable:
+		return
+	
+	var collision_vector: Vector2 = collision.get_normal()
+
+	if collision_vector.y < -0.7:
+		# damage to ennemi and jump on it
+		enemy.get_damage()
+	else:
+		# damage to player and bounce back
+		health -= 1
+		is_hit = true
+		print("player life:", health)
+		if health <= 0:
+			get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+			
+	is_invulnerable = true
+	damage_cooldown_timer.start()
+	velocity = collision_vector * DAMAGE_BOUNCE
+
+
+func _on_damage_cooldown_timer_timeout():
+	is_invulnerable = false
+	if is_hit:
+		is_hit = false
